2068070cfe2ba2cfc9ffbcf5944d719e1ae555b1
Daniel Barba <frdb103@gmail.com>
2023-02-12 18:49:55 -0500

    running

--- functions/src/dbConnect.js
+++ functions/src/dbConnect.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 import { service_acccount } from "../CONNECT.js";
 
-export default function dbConnect(){
+export default function dbConnect() {
     const client = new MongoClient(service_acccount)
     const db = client.db("affirmation-db")
     return db
